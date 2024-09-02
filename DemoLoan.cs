d947bbdb30ec4a7be9b4a6a660707a08d7d23bfc
jrobin5993 <jrobin5993@my.sullivan.edu>
2024-09-01 23:44:38 -0400

    Step 1 : Create class contains three auto-implemented properties for the loan number, the customer's last name, and the loan's value.

---
+++ DemoLoan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC242_07_01_DemoLoan_jmr
+{
+    internal class DemoLoan
+    {
+    }
+}
